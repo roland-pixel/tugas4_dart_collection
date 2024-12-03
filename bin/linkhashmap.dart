261f8e28cc05cc15f84a268ab07deecf51e04341
roland-pixel <kharis7181@gmail.com>
2024-12-03 15:29:36 +0700

    linkhashmap

---
+++ bin/linkhashmap.dart
@@ -0,0 +1,13 @@
+import 'dart:collection';
+
+void main() {
+  final scores = LinkedHashMap<String, int>();
+
+  scores["kharis"] = 100;
+  scores["mamat"] = 100;
+  scores["budi"] = 100;
+  scores["wawan"] = 100;
+  scores["wati"] = 100;
+
+  print(scores);
+}
