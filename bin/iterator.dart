9e2e38ff66ab150d886d015695d8303065d1ffae
roland-pixel <kharis7181@gmail.com>
2024-12-03 14:25:15 +0700

    iterasi manual

--- bin/iterator.dart
+++ bin/iterator.dart
@@ -1,7 +1,14 @@
 void main() {
-  var names = ['seth', 'logan', 'mack'];
+  // var names = ['seth', 'logan', 'mack'];
 
-  for (var value in names) {
-    print(value);
+  // for (var value in names) {
+  //   print(value);
+  // }
+
+  var names = ['seth', 'Logan', 'mack'];
+  var iterator = names.iterator;
+
+  while (iterator.moveNext()) {
+    print(iterator.current);
   }
 }
